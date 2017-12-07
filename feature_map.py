e12e189f48a169bd929fdf194686ce95b64c1db3
Liya Ji <lji@lenovo.com>
2017-12-07 12:41:42 +0800

    add the new feature_map

--- feature_map.py
+++ feature_map.py
@@ -1,5 +1,26 @@
+from unidecode import unidecode
+
 def feature_map(file_name,features):
     outfile = open(file_name, 'w')
     for i, feat in enumerate(features):
         outfile.write('{0}\t{1}\tq\n'.format(i, feat.encode('u8')))
     outfile.close()
+
+def feature_map_w_load_file(infile_name, outfile_name):
+    infile = open(infile_name, 'r+')
+    outfile = open(outfile_name, 'w')
+    for elm in infile.read().split('\n')[:-1]:
+        v = elm.split('\t')
+        #print v[1].decode('u8').encode('unicode-escape')
+        tmp = ""
+        for elm in unidecode(v[1].decode('u8')).split(' ')[:-1]:
+            tmp += str(elm) + '_'
+
+        outfile.write('{0}\t{1}\tq\n'.format(v[0], tmp[:-1]))
+    infile.close()
+    outfile.close()
+
+if __name__ == "__main__":
+    feature_map_w_load_file('xgb.fmap', 'xgb_uni.fmap')
+        
+
